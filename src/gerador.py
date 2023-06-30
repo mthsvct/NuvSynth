74a149ba36202528342790193315d55b1cf1cb0e
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-06-30 11:42:15 -0300

    Valores de cpu possíveis

--- src/gerador.py
+++ src/gerador.py
@@ -4,8 +4,21 @@ import string
 
 class Gerador:
 
-    def __init__(self, num, name=None) -> None:
-        self.num = num
+    def __init__(self, numProviders, name=None) -> None:
+        self.numProviders = numProviders
+        self.rams = [4, 8, 16, 32, 64, 128, 256, 512]
+        self.cpus = [
+            'Intel Core 3', 
+            'Intel Core 5', 
+            'Intel Core 7', 
+            'Intel Core 9', 
+            'AMD Ryzen 5', 
+            'AMD Ryzen 7', 
+            'AMD Ryzen 9', 
+            'Qualcomm Snapdragon', 
+            'Apple A14 Bionic', 
+            'Apple M1', 
+            'AMD Threadripper']
         if name == None or name == '':
             self.name = ''.join( random.choice(string.ascii_letters+string.digits) for _ in range(50) )
         else:
