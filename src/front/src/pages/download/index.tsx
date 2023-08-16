503b6a1b0c025cf708d658967778460e9c043274
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-08-16 20:57:53 -0300

    Icone acrescentado

--- src/front/src/pages/download/index.tsx
+++ src/front/src/pages/download/index.tsx
@@ -6,6 +6,9 @@ import { GetServerSideProps, NextPage } from 'next';
 import fs from 'fs';
 import path from 'path';
 
+import { BsDownload } from "react-icons/bs";
+import { Button } from "@/components/button";
+
 const DownloadPage: NextPage<{ data: any, nameFile:string }> = ({ data, nameFile }) => {
 	const handleDownload = () => {
 		const jsonData = JSON.stringify(data, null, 2);
@@ -20,6 +23,7 @@ const DownloadPage: NextPage<{ data: any, nameFile:string }> = ({ data, nameFile
 
 	return (
 		<div className={styles.download} onClick={handleDownload}>
+			<BsDownload />
 			<h1>Download</h1>
 		</div>
 	);
@@ -77,10 +81,15 @@ export default function Download(
 			<div className={styles.conteudo}>
 
 				<div className={styles.completo}>
-					<span>Download de todos os dados gerados</span>
-					<DownloadPage data={pvrs} nameFile={`${nome.replace(" ", "_")}.json`} />
+					<div className={styles.ajuste}>
+						<span>Download de todos os dados gerados:</span>
+						<DownloadPage data={pvrs} nameFile={`${nome.replace(" ", "_")}.json`} />
+					</div>
 				</div>
 
+				<div className={styles.titulozinho}>
+					<span>Download de provedores separados:</span>
+				</div>
 				<div className={styles.separado}>
 
 					{
@@ -102,7 +111,12 @@ export default function Download(
 					}
 				</div>
 
+				<Button>Voltar</Button>
+
 			</div>
+
+			
+
 		</>
 	)
 }
